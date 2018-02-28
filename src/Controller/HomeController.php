7abf9177de15697651ae6ff974ce29dfe3e8db06
viher3 <viher_3@hotmail.com>
2018-02-28 15:05:04 +0100

    HomeCtrl

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+	namespace App\Controller;
+
+	use Symfony\Component\HttpFoundation\JsonResponse;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+	class HomeController extends Controller
+	{
+		public function index()
+		{
+			return new JsonResponse("Nottes api!");
+		}
+	}
