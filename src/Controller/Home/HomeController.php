24f6207157617ac59f76bb20d0a5f03c5913f829
viher3 <viher_3@hotmail.com>
2018-03-04 12:56:13 +0100

    HomeCtrl moved

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-	namespace App\Controller;
+	namespace App\Controller\Home;
 
 	use Symfony\Component\HttpFoundation\JsonResponse;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
