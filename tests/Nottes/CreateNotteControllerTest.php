b92a78fdec44788a466b54e76164a70ce113573f
viher3 <viher_3@hotmail.com>
2018-03-09 20:38:07 +0100

    CreateNotteCtrlTest init

--- tests/Nottes/CreateNotteControllerTest.php
+++ tests/Nottes/CreateNotteControllerTest.php
@@ -3,15 +3,33 @@
 	namespace App\Tests\Nottes;
 
 	use App\Entity\Notte;
-	use PHPUnit\Framework\TestCase;
+	use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 	use App\Tests\AuthenticationHelper;
 
-	class CalculatorTest extends TestCase
+	class CreateNotteControllerTest extends WebTestCase
 	{
 	    public function testAdd()
 	    {
-	        $authToken = ( new AuthenticationHelper() ) ->getAuthToken();
+	        $authToken 	= ( new AuthenticationHelper() ) ->getAuthToken();
+	        $client 	= static::createClient();
 
-	        
+	        $client->request(
+		      "POST",
+		      "/api/notte",
+		      [
+		        "name" => "test",
+		        "content" => "phpunit-test",
+		        "tags" => "phpunit,test",
+		        "isEncrypted" => false
+		      ],
+		      [],
+		      [
+		        'HTTP_AUTHORIZATION' => $authToken
+		      ]
+		    );
+
+		    $arrTestResult = json_decode( $client->getResponse()->getContent(), true );
+
+		    $this->assertEquals(200, $client->getResponse()->getStatusCode());
 	    }
 	}
