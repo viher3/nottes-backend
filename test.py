0dc9174a7131de59bbc4c3452e526b9a62d2dc3c
viher3 <viher_3@hotmail.com>
2018-03-26 01:27:30 +0200

    Local test python script

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+
+print "Testing Nottes backend ..."
+
+appPath	= os.getcwd()
+
+# commands 
+testCmds = [
+	'php bin/console doctrine:schema:drop --force',
+	'php bin/console doctrine:schema:create',
+	'php bin/console doctrine:fixtures:load --append',
+	'php vendor/bin/simple-phpunit tests'
+]
+
+# excuting commands
+for cmd in testCmds:
+	os.system(cmd)
