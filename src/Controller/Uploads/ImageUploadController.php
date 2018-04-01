df8a5128eb655f7f88c5775d7ed1b40e9e53fbb0
viher3 <viher_3@hotmail.com>
2018-04-01 19:53:17 +0200

    ImageUploadCtrl init

---
+++ src/Controller/Uploads/ImageUploadController.php
@@ -0,0 +1,36 @@
+<?php
+
+	namespace App\Controller\Uploads;
+
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\Routing\Annotation\Route;
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpFoundation\Response;
+
+	use FOS\RestBundle\View\View;
+	use Nelmio\ApiDocBundle\Annotation\Model;
+	use Swagger\Annotations as SWG;
+	use App\Entity\Notte;
+
+	class ImageUploadController extends Controller
+	{
+		/**
+	     * Upload an image
+	     *
+	     * @Route("/upload/image", name="upload_image", methods={"GET"}).
+	     *
+	     * @SWG\Response(
+	     *     response=200,
+	     *     description="Upload an image"
+	     * )
+	     * @SWG\Tag(name="uploads")
+	     */
+		public function index(Request $request)
+		{
+			$droplet = $this->container->get('do.factory')->droplet();
+
+			var_dump($droplet);
+
+    		return View::create([], Response::HTTP_INTERNAL_SERVER_ERROR, []);
+		}
+	}
