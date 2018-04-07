2535cc6a9a2f5a17abcd0e1d2cd3b3b9aee76bd4
viher3 <viher_3@hotmail.com>
2018-04-07 14:36:19 +0200

    FileSanitizerTest

---
+++ tests/Services/Upload/FileSanitizerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+	namespace App\Tests\Services\Upload;
+
+	use PHPUnit\Framework\TestCase;
+	use App\Services\Upload\FileSanitizer;
+
+	class FileSanitizerTest extends TestCase
+	{
+		public function testSanitize()
+		{
+			$fileSanitizer = new FileSanitizer();
+			$result = $fileSanitizer->sanitize("ñññ");
+
+			$this->assertStringEndsWith("n", $result);
+		}
+	}
