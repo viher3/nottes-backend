948e4a1d8440badfed3e2e1f21a4e767bd9e2acf
viher3 <viher_3@hotmail.com>
2018-04-29 16:03:11 +0200

    testUpdateEntityError() in update general config test

--- tests/Configuration/UpdateGeneralConfigurationTest.php
+++ tests/Configuration/UpdateGeneralConfigurationTest.php
@@ -51,4 +51,50 @@
 		    $this->assertEquals(422, $client->getResponse()->getStatusCode());
 		    $this->assertNotEmpty($result["form"]["children"]);
 	    }
+
+	    public function testInvalidPassword()
+	    {
+	    	$client	= static::createClient();
+	        $client	= ( new AuthenticationHelper() )->getAuthToken($client);
+
+	        $client->request(
+		      "PUT",
+		      "/api/configuration/general",
+		      [
+		      	"nickname" => "test nickname",
+		      	"email" => "phpunit@general-configuration.com",
+		      	"language" => "es",
+		      	"password" => 123456
+		      ]
+		    );
+
+		    $result = $client->getResponse()->getContent();
+		    $result = json_decode($result, true);
+
+		    $this->assertEquals(500, $client->getResponse()->getStatusCode());
+		    $this->assertEquals($result['error'], "invalid_password");
+	    }
+
+	    public function testUpdateEntityError()
+	    {
+	    	$client	= static::createClient();
+	        $client	= ( new AuthenticationHelper() )->getAuthToken($client);
+
+	        $client->request(
+		      "PUT",
+		      "/api/configuration/general",
+		      [
+		      	"nickname" => new \stdClass(),
+		      	"email" => "phpunit",
+		      	"language" => "es",
+		      	"password" => 1234
+		      ]
+		    );
+
+		    $result = $client->getResponse()->getContent();
+		    $result = json_decode($result, true);
+
+		    $this->assertEquals(500, $client->getResponse()->getStatusCode());
+		    $this->assertNotEmpty($result['error']);
+	    }
 	}
