4ce2b42e05bb419ab607319e6272212214c053fd
viher3 <viher_3@hotmail.com>
2018-05-02 23:10:37 +0200

    create doc test: testErrorSavingEntity() and testInvalidForm() method

--- tests/Nottes/CreateNotteControllerTest.php
+++ tests/Nottes/CreateNotteControllerTest.php
@@ -30,6 +30,48 @@
 		    $this->assertNotEmpty($result['id']);
 	    }
 
+	    public function testInvalidForm()
+	    {
+	        $client	= static::createClient();
+	        $client	= ( new AuthenticationHelper() )->getAuthToken($client);
+
+	        $client->request(
+		      "POST",
+		      "/api/notte",
+		      [
+		        "name" => "test"
+		      ]
+		    );
+
+		    $result = json_decode( $client->getResponse()->getContent(), true );
+
+		    $this->assertEquals(500, $client->getResponse()->getStatusCode());
+		    $this->assertNotEmpty($result['form']['children']);
+	    }
+
+	    public function testErrorSavingEntity()
+	    {
+	        $client	= static::createClient();
+	        $client	= ( new AuthenticationHelper() )->getAuthToken($client);
+
+	        $client->request(
+		      "POST",
+		      "/api/notte",
+		      [
+		        "name" => new \stdClass(),
+		        "content" => "phpunit-test",
+		        "tags" => "phpunit,test",
+		        "isEncrypted" => false
+		      ]
+		    );
+
+		    $result = json_decode( $client->getResponse()->getContent(), true );
+
+		    $this->assertEquals(500, $client->getResponse()->getStatusCode());
+		    $this->assertNotEmpty($result['ERROR']);
+		    $this->assertEquals($result['ERROR'], "SAVING_DATA");
+	    }
+
 	    public function testEncrytion()
 	    {
 	        $client	= static::createClient();
