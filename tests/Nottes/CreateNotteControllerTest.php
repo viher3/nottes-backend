6dc0695ca7e521626ab0de765ad4e373fb6c1f76
viher3 <viher_3@hotmail.com>
2018-05-02 22:47:17 +0200

    CreateNotteTest testEncryption

--- tests/Nottes/CreateNotteControllerTest.php
+++ tests/Nottes/CreateNotteControllerTest.php
@@ -29,4 +29,27 @@
 		    $this->assertEquals(200, $client->getResponse()->getStatusCode());
 		    $this->assertNotEmpty($result['id']);
 	    }
+
+	    public function testEncrytion()
+	    {
+	        $client	= static::createClient();
+	        $client	= ( new AuthenticationHelper() )->getAuthToken($client);
+
+	        $client->request(
+		      "POST",
+		      "/api/notte",
+		      [
+		        "name" => "test",
+		        "content" => "phpunit-test",
+		        "tags" => "phpunit,test",
+		        "isEncrypted" => true,
+		        "encryptionpwd2" => "1234"
+		      ]
+		    );
+
+		    $result = json_decode( $client->getResponse()->getContent(), true );
+
+		    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+		    $this->assertNotEmpty($result['id']);
+	    }
 	}
