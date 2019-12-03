a67bd2775a15a59340dccbba9ebcd732aed37a66
viher3 <viher_3@hotmail.com>
2019-12-03 13:47:22 +0100

    Fixed entity repositories

--- src/Repository/NotteRepository.php
+++ src/Repository/NotteRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use App\Entity\Notte;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Notte|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class NotteRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Notte::class);
     }
