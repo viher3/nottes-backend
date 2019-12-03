274ce1cad853cf81a65b5ed5e6a2fb2aeb085269
viher3 <viher_3@hotmail.com>
2019-12-03 13:45:47 +0100

    Fix DocumentRepository

--- src/Repository/DocumentRepository.php
+++ src/Repository/DocumentRepository.php
@@ -4,11 +4,11 @@ namespace App\Repository;
 
 use App\Entity\Document;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 class DocumentRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Document::class);
     }
